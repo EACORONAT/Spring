5e170f948098aaf54abe244a382cfa210d3b1b2f
Eduardo C. <109992322+EACORONAT@users.noreply.github.com>
2024-07-17 13:01:56 -0600

    Add files via upload

---
+++ springboot-jpa/target/classes/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO `personas` (`apellido`, `lenguaje_prog`, `nombre`) VALUES ('Gomez', 'Python', 'Melina');
+INSERT INTO `personas` (`apellido`, `lenguaje_prog`, `nombre`) VALUES ('Corona', 'Java', 'Eduardo');
+INSERT INTO `personas` (`apellido`, `lenguaje_prog`, `nombre`) VALUES ('Corona', 'JavaScript', 'Jesus');
+INSERT INTO `personas` (`apellido`, `lenguaje_prog`, `nombre`) VALUES ('Torres', 'C#', 'Antonio');
+INSERT INTO `personas` (`apellido`, `lenguaje_prog`, `nombre`) VALUES ('Marin', 'Ruby', 'Yeudiel');
