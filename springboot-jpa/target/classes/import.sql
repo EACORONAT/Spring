2be10fd97b60cb34bbdc33a125b3c857d0a38f6d
Eduardo C. <109992322+EACORONAT@users.noreply.github.com>
2024-07-17 12:51:06 -0600

    Delete springboot-jpa directory

--- springboot-jpa/target/classes/import.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `personas` (`apellido`, `lenguaje_prog`, `nombre`) VALUES ('Gomez', 'Python', 'Melina');
-INSERT INTO `personas` (`apellido`, `lenguaje_prog`, `nombre`) VALUES ('Corona', 'Java', 'Eduardo');
-INSERT INTO `personas` (`apellido`, `lenguaje_prog`, `nombre`) VALUES ('Corona', 'JavaScript', 'Jesus');
-INSERT INTO `personas` (`apellido`, `lenguaje_prog`, `nombre`) VALUES ('Torres', 'C#', 'Antonio');
-INSERT INTO `personas` (`apellido`, `lenguaje_prog`, `nombre`) VALUES ('Marin', 'Ruby', 'Yeudiel');
